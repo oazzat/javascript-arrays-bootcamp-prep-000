fca1fa4697fbb925cb951cfc206af1cf44992294
Omar Azzat <oazzat19@gmail.com>
2018-09-06 03:29:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,11 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
+function addElementToBeginningOfArray(theArray,elementToAdd){
+  var newArray = [elementToAdd, ...theArray]
+  return theArray
+}
+
+function destructivelyAddElementToBeginningOfArray(theArray,elementToAdd){
+ 
+ return theArray 
+}
